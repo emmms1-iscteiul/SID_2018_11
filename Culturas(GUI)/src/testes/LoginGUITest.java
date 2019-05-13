19f40bd428295ca2c0fb1c4c81a23dfb74f6eb35
emmms1-iscteiul <emmms1@iscte-iul.pt>
2019-05-13 15:38:54 +0100

    Teste LoginGUI acrescentado

---
+++ Culturas(GUI)/src/testes/LoginGUITest.java
@@ -0,0 +1,38 @@
+package testes;
+
+import static org.junit.Assert.*;
+
+import javax.swing.JTextField;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import gui.LoginGui;
+/**
+ * Teste GUI Login
+ * @author Eduardo
+ *
+ */
+public class LoginGUITest {
+
+	LoginGui test = new LoginGui("Login");
+/**
+ * Teste verificar user	
+ */
+	@Test
+	public void testCheckUser() {
+		JTextField usernameText = new JTextField("User");
+		String output = test.checkUser(usernameText );
+		Assert.assertNotNull(usernameText);
+		Assert.assertNotNull(output);
+	}
+
+	@Test
+	public void testCheckUserPass() {
+		JTextField usernameText = new JTextField("Password");
+		String output = test.checkUserPass(usernameText);
+		Assert.assertNotNull(usernameText);
+		Assert.assertNotNull(output);
+	}
+	
+}
